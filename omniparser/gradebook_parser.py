ad20bf6706d2059f37aa56195c2c02ce32180c97
Preetibhatia <50919836+Preetibhatia@users.noreply.github.com>
2019-06-10 20:56:16 -0400

    Update gradebook_parser.py

--- omniparser/gradebook_parser.py
+++ omniparser/gradebook_parser.py
@@ -1,2 +1,19 @@
+# omniparser/gradebook_parser.py
 
-print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
+import pandas
+import os
+
+def calculate_average_grade_from_csv(my_csv_filepath):
+    df = pandas.read_csv(my_csv_filepath)
+
+    breakpoint()
+
+    return 90.64 #"OOPS"
+
+
+if __name__ == "__main__":
+    print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
+
+    gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2019.csv")
+
+    calculate_average_grade_from_csv(gradebook_filepath)
